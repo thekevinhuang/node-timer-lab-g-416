5f3bd8d37235fab1d1d0482e4a0f981ead62007e
Kevin Huang <yellow.khaos@gmail.com>
2019-07-16 22:55:56 +0000

    Automatically backed up by Learn

--- timer.js
+++ timer.js
@@ -5,10 +5,15 @@ if (!input[2]) {
   console.log("Please enter some time")
   process.exit(1)
 } else if (input[2].indexOf('s')>-1) {
-
+  timeleft = parseInt(input[2].slice(0,-1),10)
 } else if (input[2].indexOf('min')>-1) {
-
+  timeleft = parseInt(input[2].slice(0,-3),10*60)
 } else {
   console.log("Please enter a valid time")
   process.exit(1)
 }
+
+for (i=timeleft; i>0; i--) {
+  process.setInterval(()=>{console.log(`Left: ${i}s`)})
+}
+
