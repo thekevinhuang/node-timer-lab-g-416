07185e1f22abfa50c3f39b30610e9d181b8df1a9
Kevin Huang <yellow.khaos@gmail.com>
2019-07-16 22:48:42 +0000

    Automatically backed up by Learn

---
+++ timer.js
@@ -0,0 +1,14 @@
+let timeleft = 0
+let input = process.argv
+
+if (!input[2]) {
+  console.log("Please enter some time")
+  process.exit(1)
+} else if (input[2].indexOf('s')>-1) {
+  
+} else if (input[2].indexOf('min')>-1) {
+  
+} else {
+  console.log("Please enter a valid time")
+  process.exit(1)
+}
